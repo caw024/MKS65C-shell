9004957ee89f6f4775a526dc855a76a10c547a9a
caw024 <caw@stuy.edu>
2018-11-19 15:19:24 -0500

    k

---
+++ shell.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <string.h>
+
+int main(){
+}
