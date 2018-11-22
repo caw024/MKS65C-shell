844648f1a8855d5879ab4cdf8a07ceb9fbafcfa8
Calvin Aw <caw@stuy.edu>
2018-11-22 12:28:59 -0500

    k

--- shell.h
+++ shell.h
@@ -1,10 +1,14 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-//parses semicolons
+//return type: char ** (pointer to a char pointer)
+//arguments: char * (a string)
+//fxn: parses the semicolons in a given line to get commands
 char ** parse_argssemi(char *line);
 
-//parses spaces
+//return type: char ** (pointer to a char pointer)
+//arguments: char * (a string)
+//fxn: parses spaces in a given line to get arguments
 char ** parse_argsspace(char *line);
 
 #endif
